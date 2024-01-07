9fb1bc97752989b2e036990c67eae85be264c5bb
Koval0050 <121100028+Koval0050@users.noreply.github.com>
2024-01-07 19:17:23 +0200

    fixed the style queue

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -4,6 +4,8 @@ import "../../scss/index";
 // components
 import "../../components/header/index";
 import "../../components/main/index"
+import '../../components/search-input/index'
+import '../../components/cards-list/index'
 import "../../components/footer/index";
 
 // page-components
