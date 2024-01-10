7a6f27e17403622ac4c5463bd5d693fec709c774
Koval0050 <121100028+Koval0050@users.noreply.github.com>
2024-01-10 15:21:50 +0200

    add discount

---
+++ src/api/api.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const baseURL = "https://659d33da633f9aee7908e266.mockapi.io/products";
+
+export const getMenu = async () => {
+  try {
+    const response = await axios.get(baseURL);
+    return response.data
+  } catch (error) {
+    console.error("Error fetching", error);
+    throw error;
+  }
+};
